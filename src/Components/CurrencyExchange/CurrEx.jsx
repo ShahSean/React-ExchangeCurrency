c8a1171a2a67b4b6a833f8afc0a426c941224f5a
Shahryar Shagoshtasbi <shahryar9782@gmail.com>
2020-08-04 12:30:34 -0400

    Rate Bug Fixed, API Based Currency added

--- src/Components/CurrencyExchange/CurrEx.jsx
+++ src/Components/CurrencyExchange/CurrEx.jsx
@@ -10,6 +10,7 @@ export default class CurrEx extends Component {
 
     this.state = {
       userInput: 0,
+      rate: 1,
       rates: {},
       firstSelectedCurrency: "USD",
       secondSelectedCurrency: "CAD",
@@ -21,25 +22,24 @@ export default class CurrEx extends Component {
     };
   }
 
-  getRate(fromCurrency, toCurrency) {
-    const { rates } = this.state;
-    // String manipulation
-    const currency_key = `${fromCurrency}->${toCurrency}`;
-    if (currency_key in rates) return rates[currency_key];
-    else {
-      // TODO: update the currency bank for the current value
-      //       if there is another such fetch in the works, don't run it again
-      return 1;
-    }
-  }
+  // getRate(fromCurrency, toCurrency) {
+  //   const { rates } = this.state;
+  //   // String manipulation
+  //   const currency_key = `${fromCurrency}->${toCurrency}`;
+  //   if (currency_key in rates) return rates[currency_key];
+  //   else {
+  //     // TODO: update the currency bank for the current value
+  //     //       if there is another such fetch in the works, don't run it again
+  //     return 1;
+  //   }
+  // }
 
   componentDidMount() {
-    this.updateRate();
+    this.updateRate(
+      this.state.firstSelectedCurrency,
+      this.state.secondSelectedCurrency
+    );
   }
-
-  getInitialSelectFirst = () => {
-    return this.state.firstSelectedCurrency;
-  };
 
   render() {
     return (
@@ -97,27 +97,37 @@ export default class CurrEx extends Component {
   }
   firstCurrencySelectionHandler = (e) => {
     this.setState({ firstSelectedCurrency: e.target.value });
-    this.updateRate();
+    console.log("This is the  first selction: ", e.target.value);
+    this.updateRate(e.target.value, this.state.secondSelectedCurrency);
   };
   secondCurrencySelectionHandler = (e) => {
     this.setState({ secondSelectedCurrency: e.target.value });
+    console.log("This is the 2nd selction: ", e.target.value);
+
+    this.updateRate(this.state.firstSelectedCurrency, e.target.value);
   };
 
   // This Function gets the latest rate from European Central Bank API
   // https://exchangeratesapi.io/
-  updateRate() {
+  // To change the base
+  //https://api.exchangeratesapi.io/latest?symbols=USD,GBP&base=JPY
+  updateRate(fromCurrency, toCurrency) {
     axios
       .get(
         "https://api.exchangeratesapi.io/latest?symbols=" +
-          this.state.firstSelectedCurrency +
-          "," +
-          this.state.secondSelectedCurrency
+          toCurrency +
+          "&base=" +
+          fromCurrency
       )
       .then((result) => {
-        this.setState({ rate: Object.values(result.data.rates)[1] });
+        console.log("1st: ", this.state.firstSelectedCurrency);
+        console.log("2nd: ", this.state.secondSelectedCurrency);
+        console.log("It's", result.data.rates);
+        console.log("boo", Object.values(result.data.rates)[0]);
+        this.setState({ rate: Object.values(result.data.rates)[0] });
       });
   }
-
+  // Simpel Coversion based on the rate
   convertor = () => {
     let userInput = this.state.userInput;
     return userInput * this.state.rate;
